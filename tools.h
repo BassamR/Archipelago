46bd770460236c30162b3f3d5f1b578085228b00
BassamR <bassamr99@gmail.com>
2020-03-08 16:57:54 +0100

    add header tags

--- tools.h
+++ tools.h
@@ -1,3 +1,14 @@
+#ifndef TOOLS_HEADER_H
+#define TOOLS_HEADER_H
+
 double distancePoints();
 double distancePointVecteur();
 double norme();
+
+struct Cercle {
+    int x;
+    int y;
+    int r;
+}; //temporary, just to be able to use the type Cercle in other modules
+
+#endif
--- ville.h
+++ ville.h
@@ -1,1 +1,6 @@
+#ifndef VILLE_HEADER_H
+#define VILLE_HEADER_H
+
 void lecture(char* nomFichier);
+
+#endif
