2ef9f609d0318e90d431de79a0cbf53a4943fa2e
BassamR <bassamr99@gmail.com>
2020-03-09 21:12:27 +0100

    add tools module

--- tools.cc
+++ tools.cc
@@ -4,3 +4,43 @@
 #include "tools.h"
 #include "constantes.h"
 using namespace std;
+
+void cvecteur(coordo a, coordo b, vecteur v) {
+     v.a = b.x-a.x;
+     v.b = b.y-a.y;
+}
+
+double norme(vecteur v) {
+    return sqrt(v.a*v.a+v.b*v.b);
+}
+
+bool intersectioncc(cercle p, cercle l) {
+    vecteur cc;
+    cvecteur(p.centre, l.centre, cc);
+    double dist = norme(cc);
+    if (p.rayon+l.rayon+dist_min >= dist) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool intersectioncs(cercle c, cercle s, cercle t) {
+    vecteur cs; //creation du vecteur allant du centre de c au centre de s
+    cvecteur(c.centre, s.centre, cs);
+
+    vecteur ct;
+    cvecteur(c.centre, s.centre, ct);
+
+    vecteur cthort; //vecteur orthogonal au segment cs
+    double produit = (ct.a*cs.a+ct.b*cs.b)/(cs.a*cs.a+cs.b*cs.b); //produit scalaire/norme
+    cthort.a = ct.a-produit*cs.a; // orthogonalisation
+    cthort.b = ct.b-produit*cs.b; //ortho
+    double dist = norme(cthort); //distance minimale entre le centre de t et le segment cs
+
+    if(dist <= t.rayon+dist_min) {
+        return true;
+    } else {
+        return false;
+    }
+}
--- tools.h
+++ tools.h
@@ -1,6 +1,8 @@
 #ifndef TOOLS_HEADER_H
 #define TOOLS_HEADER_H
 
+#include "constantes.h"
+
 double distancePoints();
 double distancePointVecteur();
 double norme();
@@ -8,6 +10,32 @@ double norme();
 struct Cercle {
     unsigned int x;
     unsigned int y;
-}; //temporary, just to be able to use the type Cercle in other modules
+};
+//above is temporary, just to be able to use the type Cercle in other modules
+
+struct coordo {
+     double x;
+     double y;
+};
+
+struct vecteur {
+     double a;
+     double b;
+};
+
+struct cercle {
+     coordo centre;
+     double rayon;
+};
+
+struct segment {
+     coordo p;
+     vecteur v;
+};
+
+void cvecteur(coordo a, coordo b, vecteur v);
+double norme(vecteur v);
+bool intersectioncc(cercle p, cercle l);
+bool intersectioncs(cercle c, segment s);
 
 #endif
