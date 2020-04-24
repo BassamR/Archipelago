f08aa13092bb6851cb10a8632fae2bfc701b33fd
BassamR <bassamr99@gmail.com>
2020-04-24 17:22:02 +0200

    get rid of useless includes and using namespace std

--- projet.cc
+++ projet.cc
@@ -5,11 +5,9 @@
 * \version 1.0
 */
 
-#include <iostream>
 #include <gtkmm/application.h>
 #include "ville.h"
 #include "gui.h"
-using namespace std;
 
 int main(int argc, char* argv[]) {
     int fakeArgc = 1; //tricks Gtk into thinking argc=1
